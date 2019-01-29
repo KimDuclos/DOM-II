8b9e965491c0a16c995444f264eb5e4cc79efe15
Kim Duclos <kimduclos@gmail.com>
2019-01-29 14:12:18 -0700

    added mouseover and mouseout on header image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+// mouseover & mouseout -- changes header image
+
+var changeHeader = document.querySelector('img');
+
+changeHeader.onmouseout = function () {
+    this.src = "img/fun-bus.jpg";
+}
+changeHeader.onmouseover = function () {
+    this.src = "img/miss-frizzle.jpg";
+}
+
