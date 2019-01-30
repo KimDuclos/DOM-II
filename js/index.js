4481cf2ec18733394a5a1e3908a5728e53417553
Kim Duclos <kimduclos@gmail.com>
2019-01-29 18:09:52 -0700

    added a form with triggered alert window upon submission, started trying animations

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
-// mouseover -- changes header image to Ms. Frizzle
-// mouseout -- changes lets image go back to Fun Bus
+// 1. mouseover -- changes header image to Ms. Frizzle
+// 2. mouseout -- changes lets image go back to Fun Bus
 
 const changeHeader = document.querySelector('img');
 
@@ -14,21 +14,21 @@ changeHeader.addEventListener('mouseover', () => {
 });
 
 
-// click - change first button color 
+// 3. click - change first button color 
 let changeHomeBtn = document.getElementById('homeBtn');
     changeHomeBtn.addEventListener('click', e => {
     changeHomeBtn.style.backgroundColor = 'red';
 });
 
 
-// double click - change size of second button
+// 4. double click - change size of second button
 let changeAboutBtn = document.getElementById('aboutBtn');
     changeAboutBtn.addEventListener('dblclick', e => {
     changeAboutBtn.style.borderColor = "blue";    
 });
 
 
-// keydown shrinks blog button
+// 5. keydown shrinks blog button
 let changeBlogBtn = document.getElementById('blogBtn');
     changeBlogBtn.addEventListener('keydown', e => {
     changeBlogBtn.style.width = "0px";   
@@ -36,23 +36,33 @@ let changeBlogBtn = document.getElementById('blogBtn');
 });
 
 
-// mouse wheel turns contact button black
+// 6. mouse wheel turns contact button black
 let changeContactBtn = document.getElementById('contactBtn');
     changeContactBtn.addEventListener('wheel', e => {
     changeContactBtn.style.backgroundColor = "black";   
 });
 
 
-// changes the font color of the first sign up button when the mouse moves over it
+// 7. changes the font color of the first sign up button when the mouse moves over it
 let changeSignUp1 = document.getElementById('signUp1');
     changeSignUp1.addEventListener('mousemove', e => {
     changeSignUp1.style.color = "black";
 });
 
-// right clicking expands border to really huge width
+// 8. right clicking expands border to really huge width
 let changeSignUp2 = document.getElementById('signUp2');
     changeSignUp2.addEventListener('contextmenu', e => {
     changeSignUp2.style.borderWidth = "40px";
 });
 
-//
+
+// form triggers alert window when clicked
+const form = document.querySelector('.submit')
+form.addEventListener('click', e => {
+    alert('You submitted your form. Cool beans.');
+    e.stopPropagation();
+    TweenMax.to(e.currentTarget, 1, {
+        width:200,
+        ease:Bounce.easeOut
+      });
+});
