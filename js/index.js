212b54606d0f0a6c7de3ba08ed661626a37a0630
Kim Duclos <kimduclos@gmail.com>
2019-01-29 17:15:52 -0700

    updated header, home and about buttons

--- js/index.js
+++ js/index.js
@@ -2,12 +2,27 @@
 
 // mouseover & mouseout -- changes header image
 
-var changeHeader = document.querySelector('img');
+const changeHeader = document.querySelector('img');
 
-changeHeader.onmouseout = function () {
-    this.src = "img/fun-bus.jpg";
-}
-changeHeader.onmouseover = function () {
-    this.src = "img/miss-frizzle.jpg";
-}
+changeHeader.addEventListener('mouseout', () => {
+    changeHeader.src = "img/fun-bus.jpg"
+}); 
+
+changeHeader.addEventListener('mouseover', () => {
+    changeHeader.src = "img/miss-frizzle.jpg"
+});
+
+
+// click - change first button color 
+let changeHomeBtn = document.getElementById('homeBtn');
+    changeHomeBtn.addEventListener('click', e => {
+    changeHomeBtn.style.backgroundColor = 'red';
+});
+
+
+// double click - change size of second button
+let changeAboutBtn = document.getElementById('aboutBtn');
+    changeAboutBtn.addEventListener('dblclick', e => {
+    changeAboutBtn.style.borderColor = "blue";    
+});
 
