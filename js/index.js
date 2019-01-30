6b63f1e970469a6f22fe5c095d35eb69eec28d7d
Kim Duclos <kimduclos@gmail.com>
2019-01-29 17:58:30 -0700

    put events on 2 of the bottom buttons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 
-// mouseover & mouseout -- changes header image
+// mouseover -- changes header image to Ms. Frizzle
+// mouseout -- changes lets image go back to Fun Bus
 
 const changeHeader = document.querySelector('img');
 
@@ -26,3 +27,32 @@ let changeAboutBtn = document.getElementById('aboutBtn');
     changeAboutBtn.style.borderColor = "blue";    
 });
 
+
+// keydown shrinks blog button
+let changeBlogBtn = document.getElementById('blogBtn');
+    changeBlogBtn.addEventListener('keydown', e => {
+    changeBlogBtn.style.width = "0px";   
+    changeBlogBtn.style.height = "0px";
+});
+
+
+// mouse wheel turns contact button black
+let changeContactBtn = document.getElementById('contactBtn');
+    changeContactBtn.addEventListener('wheel', e => {
+    changeContactBtn.style.backgroundColor = "black";   
+});
+
+
+// changes the font color of the first sign up button when the mouse moves over it
+let changeSignUp1 = document.getElementById('signUp1');
+    changeSignUp1.addEventListener('mousemove', e => {
+    changeSignUp1.style.color = "black";
+});
+
+// right clicking expands border to really huge width
+let changeSignUp2 = document.getElementById('signUp2');
+    changeSignUp2.addEventListener('contextmenu', e => {
+    changeSignUp2.style.borderWidth = "40px";
+});
+
+//
